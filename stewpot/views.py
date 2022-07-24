47ec468fb4e928db5e55759d73072d828cf33858
jtweeder <31365056+jtweeder@users.noreply.github.com>
2022-07-24 15:16:12 -0500

    created form class for entry of share text

--- stewpot/views.py
+++ stewpot/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.urls import reverse_lazy
+from django.urls import reverse
 from django.contrib.auth.models import User
+from django.views.generic.edit import CreateView
 
 # Within App Imports
 from meals.models import mstr_recipe
@@ -9,26 +10,32 @@ from stewpot.models import share_meal
 
 # Create your views here.
 
-
 @login_required
-def share_recipe(request, meal_id, title=None, message=None):
+class share_recipe(CreateView):
+    login_required = True
+    model = share_meal
+    fields = ['title', 'text']
+    success_url = reverse('share-meal', share_meal=self.object.id)
 
-    if not title:
-        title = 'A shared recipe from MealCurator.com'
-    if not message:
-        message = 'I thought you might like this recipe I found on MealCurator.com'
-    shared_meal = share_meal.objects.create(
-                    title=title,
-                    creator=request.User,
-                    text=message,
-                    meal=mstr_recipe.objects.get(meal_id=meal_id)
-                    )
+    # TODO: FIGURE OUT FORM VALIDATION, pass object id to shared_meal url when success
 
-    return redirect('share-meal', shared_meal=shared_meal.id)
+    def form_valid(self, form):
+        shared_meal = form.save(commit=False)
+        shared_meal.creator = User.objects.get(id=self.request.user.id)
+        shared_meal.meal = mstr_recipe.objects.get(meal_id=meal_id)
+        shared_meal.save()
 
+    
+    success_url = reverse('share-meal', shared_meal.id)    
 
 def shared_meal(request, share_id):
-    context = {'shared': share_meal.objects.get(id=share_id)}
+    shared_meal = share_meal.objects.get(id=share_id)
+    sender = request.User == shared_meal.creator
+
+    context = {'shared': shared_meal,
+               'shared_view': True,
+               'sender_view': sender,
+               'meals': shared_meal.meal}
     return render(request, 'stewpot/shared.html', context)
 
 
